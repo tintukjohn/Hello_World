6eb38ce0d305547a593e538008608553d348a51a
Tintu K John <tintukjohn@gmail.com>
2020-08-10 11:04:42 +0800

    adding all files together

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		int value = new AnotherClass().firstMethod(4, 3);
+		System.out.println(value);
+	}
+
+}
